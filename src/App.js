2dbfede7dd0752af370abe15d24a2ce7be9201e1
prathamesh <prathamudeshmukh@gmail.com>
2022-10-30 18:23:07 +0530

    tools commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const getPhoneNumberLink = () => {
+    const linkPhoneNumber = phoneNumber.startsWith("+91") ? phoneNumber : `+91${phoneNumber}`
+    return `https://wa.me/${linkPhoneNumber}`
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div><label for="phoneNumber">Phone number</label></div>
+        <div><input type="text" name="phoneNumber" onChange={e => setPhoneNumber(e.target.value)}  value={phoneNumber}/></div>
+        <div><a class="App-link" href={getPhoneNumberLink()}>Open chat</a></div>
       </header>
     </div>
   );
